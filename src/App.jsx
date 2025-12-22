19753ddadb9f54e40b346e4e4574b45d7d5633cf
Raghav Taneja <raghavtaneja487@gmail.com>
2025-12-23 00:25:10 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Timeline from './components/Timeline';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div style={{ color: 'red', fontSize: '50px' }}>
-      Test App Rendering
+    <div className="app">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Timeline />
+      <Contact />
     </div>
   );
 }
